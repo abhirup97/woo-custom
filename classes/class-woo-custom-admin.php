5b566df6c8e11b24d6b90aad7705d484bd074cca
Abhirup Goswami <64842100+abhirup97@users.noreply.github.com>
2023-12-14 15:56:58 +0530

    Update class-woo-custom-admin.php

--- classes/class-woo-custom-admin.php
+++ classes/class-woo-custom-admin.php
@@ -14,12 +14,12 @@ if (!defined('ABSPATH')) {
 
 class WooCustom_Admin {
 	public function __construct() {
-		add_filter( 'woocommerce_general_settings' , [ $this, 'general_settings_shop_phone' ] );
+		add_filter( 'woocommerce_general_settings' , [ $this, 'general_settings_fee' ] );
 		// Display custom input field value into order table
 		add_filter( 'woocommerce_order_item_product', [ $this, 'woo_custom_product_add_on_display_order' ], 10, 2 );
 	}
 	
-	public function general_settings_shop_phone($settings) {
+	public function general_settings_fee($settings) {
 		$key = 0;
 
 		foreach( $settings as $values ){
@@ -29,8 +29,8 @@ class WooCustom_Admin {
 			// Inserting array just after the post code in "Store Address" section
 			if($values['id'] == 'woocommerce_price_num_decimals'){
 				$new_settings[$key] = array(
-					'title'    => __('Custom fee per order'),
-					'desc'     => __('Fee to add every order'),
+					'title'    => __('Custom fee per order', 'woo-custom' ),
+					'desc'     => __('Fee to add every order', 'woo-custom' ),
 					'id'       => 'woocommerce_custom_fee', // <= The field ID (important)
 					'default'  => '',
 					'type'     => 'number',
